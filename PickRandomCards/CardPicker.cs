f1019964a2cb6e44346fe30abf91a7f72d5e3578
eqweqjpq <98584496+eqweqjpq1999@users.noreply.github.com>
2022-02-12 00:33:17 +0800

    加入專案檔案。

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + "of" + RandomSuit();
+
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            //取得一個從1到4的隨機數字
+            int value = random.Next(1,5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            //如果都沒有 回傳Diamonds
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+
+    }
+
+}
